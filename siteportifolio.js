26f13f78103e134840bce52d26059559f1a4c938
Joaovirone <153655175+Joaovirone@users.noreply.github.com>
2023-12-12 15:46:45 -0300

    Adicionando os arquivos base
    Adicionando os arquivos base para o portifólio.

---
+++ siteportifolio.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector('.navbar');
+
+
+GamepadButton.addEventListener('click', function(){
+    mobileNavbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function(){
+    if(this.window.pageXOffset > 0) return navbar.classList.add('active')
+    return navbar.classList.remove('active');
+});
